5e3039be3e1796e8216ff6d4d3ccc994591c9a63
Julio Castillo <julio@localhost.localdomain>
2019-04-21 18:13:45 -0600

    actualizacion para el nuevo disco duro

---
+++ repository/LabSolicitudRepository.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Description of LabSolicitud
+ *
+ * @author Julio Castillo
+ */
+class LabSolicitudRepository {
+        
+    /*
+     * CARGAR SOLICITUD A TRAVES DE ID
+     */
+    function getSolicitud_id ($id_solicitud) {
+        $db = new MySQL();
+        $sql = "SELECT 
+            t01.id,
+            date_format(t01.fecha_solicitud,'%d-%m-%Y %H:%i:%s') fecha_solicitud, 
+            t01.id_paciente, 
+            t01.id_medico,
+            t03.nombre as medico,
+            t01.id_lugarentrega,
+            t01.id_servicio,
+            concat(t02.nombres,' ', t02.apellidos) paciente,
+            t02.edad,
+            t02.id_sexo,
+            t02.id_empresa,
+            t04.nombre as empresa,
+            t01.sumas,
+            t01.descuento,
+            t01.venta_total
+                     FROM lab_solicitud t01
+                        LEFT JOIN mnt_paciente t02 ON t02.id = t01.id_paciente
+                        LEFT JOIN ctl_medico t03 ON t03.id = t01.id_medico
+                        LEFT JOIN ctl_empresa t04 ON t04.id = t01.id_empresa
+                     WHERE t01.id=$id_solicitud";
+        return $db->fetch_array($db->consulta($sql));
+    }
+    
+    function getSolicitudDetalle_id($id_solicitud) {
+        $db = new MySQL();
+        $sql = "SELECT
+                    t02.id,
+                    t02.codigo,
+                    t02.nombre as prueba,
+                    t01.precio as precio_prueba
+                FROM lab_detallesolicitud  t01
+                LEFT JOIN ctl_pruebaslab t02 ON t02.id = t01.id_pruebaslab
+                WHERE t01.id_solicitud = $id_solicitud
+                ORDER BY t02.codigo";
+        return $db->consulta($sql);
+    }
+    
+}
